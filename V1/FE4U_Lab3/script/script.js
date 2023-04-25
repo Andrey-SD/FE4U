4823828d7c72fbca51df899bc5acc88270477670
Andrey-SD <andrey.sd.post@gmail.com>
2023-04-25 23:54:54 +0300

    Lab3 task1 complite(maybe)

--- V1/FE4U_Lab3/script/script.js
+++ V1/FE4U_Lab3/script/script.js
@@ -5,62 +5,78 @@ import { randomUserMock, additionalUsers } from './FE4U-Lab3-mock.js';
 
 
 
+const normalObj = (arr1, arr2) => {
+    // Данні з обєкту random-user-mock привести до вигляду: 
 
-
-const normalizeObj = (obj) => {
-    obj.full_name = `${obj.name.first} ${obj.name.last}`;
-    obj.b_date = obj.dob.date;
-    obj.b_age = obj.dob.age;
-    obj.picture_large = obj.picture.large;
-    obj.picture_thumbnail = obj.picture.thumbnail;
-    for (const key in obj) {
-        if (typeof obj[key] === "object") {
-            for (const subKey in obj[key]) {
-                obj[subKey] = obj[key][subKey];
+    const normalizeObj = (obj) => {
+        obj.full_name = `${obj.name.first} ${obj.name.last}`;
+        obj.b_date = obj.dob.date;
+        obj.b_age = obj.dob.age;
+        obj.picture_large = obj.picture.large;
+        obj.picture_thumbnail = obj.picture.thumbnail;
+        for (const key in obj) {
+            if (typeof obj[key] === "object") {
+                for (const subKey in obj[key]) {
+                    obj[subKey] = obj[key][subKey];
+                }
+                delete obj[key];
             }
-            delete obj[key];
         }
-    }
-    return obj;
-}
-
-const addProperty = (obj) => {
-
-    const makeid = (length) => {
-        let result = '';
-        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        const charactersLength = characters.length;
-        let counter = 0;
-        while (counter < length) {
-            result += characters.charAt(Math.floor(Math.random() * charactersLength));
-            counter += 1;
-        }
-        return result;
+        return obj;
     }
 
-    const courses = ['Mathematics', 'Physics', 'English', 'Computer Science', 'Dancing', 'Chess', 'Biology', 'Chemistry',
-        'Law', 'Art', 'Medicine', 'Statistics'];
+    // додати поля: id, favorite, course, bg_color,note, 
 
-    const randomTo = (max) => {
-        const min = 0;
-        max++;
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min) + min);;
+    const addProperty = (obj) => {
+
+        const makeId = (length) => {
+            let result = '';
+            const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+            const charactersLength = characters.length;
+            let counter = 0;
+            while (counter < length) {
+                result += characters.charAt(Math.floor(Math.random() * charactersLength));
+                counter += 1;
+            }
+            return result;
+        }
+
+        // список course
+
+        const courses = ['Mathematics', 'Physics', 'English', 'Computer Science', 'Dancing', 'Chess', 'Biology', 'Chemistry',
+            'Law', 'Art', 'Medicine', 'Statistics'];
+
+        const randomTo = (max) => {
+            const min = 0;
+            max++;
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min) + min);;
+        };
+
+        const newObj = {
+            id: makeId(11),
+            favorite: null,
+            course: courses[randomTo(courses.length - 1)],
+            bg_color: `#${randomTo(255).toString(16)}${randomTo(255).toString(16)}${randomTo(255).toString(16)}`,
+            note: null
+        };
+
+        return Object.assign(obj, newObj);
     };
 
-    const newObj = {
-        id: makeid(11),
-        favorite: null,
-        course: courses[randomTo(courses.length - 1)],
-        bg_color: `#${randomTo(255).toString(16)}${randomTo(255).toString(16)}${randomTo(255).toString(16)}`,
-        note: null
-    };
+    // По’єднати два обєкти позбуваючись повторів
 
-    console.log(newObj);
+    const mergeArr = (arr1, arr2) => {
+        arr1.push(arr2);
+    }
 
-};
+    for (let i = 0; i < arr1.length; i++) {
+        arr1[i] = normalizeObj(arr1[i]);
+        arr1[i] = addProperty(arr1[i]);
+    }
 
-for (let i = 0; i < randomUserMock.length; i++) {
-    randomUserMock[i] = normalizeObj(randomUserMock[i]);
-    randomUserMock[i] = addProperty(randomUserMock[i]);
+    mergeArr(arr1, arr2)
+
+    return arr1;
 }
+console.log(normalObj(randomUserMock, additionalUsers));
